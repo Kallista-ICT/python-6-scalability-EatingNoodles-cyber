c0e28927a6e3cf6e140780f4e47bd270b6a822b7
EatingNoodles-cyber <gadisannisarayhana@gmail.com>
2025-05-13 07:05:27 +0000

    Submit

---
+++ greeting.py
@@ -0,0 +1,8 @@
+def greet(name):
+    print(f"Hello, {name}! Welcome to the game!")
+
+player1 = input("What's the first player's name?")
+player2 = input("And what about you, second player?")
+
+greet(player1)
+greet(player2)
